816baa60934804d525d60b8b870e32e4967fb607
dsig0 <75738471+dsig0@users.noreply.github.com>
2025-11-09 11:10:28 +0530

    u

--- flake.nix
+++ flake.nix
@@ -5,19 +5,9 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixvim,
-      flake-utils,
-      ...
-    }@inputs:
-    let
-      config = import ./config; # import the module directly
-    in
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, nixvim, flake-utils, ... }@inputs:
+    let config = import ./config; # import the module directly
+    in flake-utils.lib.eachDefaultSystem (system:
       let
         nixvimLib = nixvim.lib.${system};
         pkgs = import nixpkgs { inherit system; };
@@ -28,8 +18,7 @@
           # You can use `extraSpecialArgs` to pass additional arguments to your module files
           extraSpecialArgs = { inherit self; };
         };
-      in
-      {
+      in {
         checks = {
           # Run `nix flake check .` to verify that your config is not broken
           default = nixvimLib.check.mkTestDerivationFromNvim {
@@ -44,6 +33,5 @@
         };
 
         formatter = pkgs.nixfmt;
-      }
-    );
+      });
 }
